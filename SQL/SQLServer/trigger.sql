589932fb2d258d6d9e9d3ae751b40a7c26628d07
gil-son <gilszon@gmail.com>
2020-12-31 05:22:04 -0300

    SQL

---
+++ SQL/SQLServer/trigger.sql
@@ -0,0 +1,49 @@
+--Sql Server 2014 Express Edition
+
+CREATE TABLE caixa (
+  data DATETIME,
+  saldo_inicial DECIMAL(10,2) NOT NULL,
+  saldo_final DECIMAL(10,2) NOT NULL)
+  GO
+
+
+--Inicialmente o Saldo Inicial é igual ao Saldo Final
+INSERT INTO caixa(data, saldo_inicial, saldo_final)
+
+-- Obtém a data, convert para DateTime e depois Convert para Vachar do Tamanho 120. Pois será preciso para comparar
+VALUES ( CONVERT(VARCHAR, CONVERT(DATETIME,GETDATE()), 120)    , 100, 100) 
+GO
+
+CREATE TABLE vendas (
+  data DATETIME,
+  cod_venda DECIMAL(10,2) NOT NULL,
+  valor DECIMAL(10,2) NOT NULL)
+GO
+  
+  
+CREATE TRIGGER tgr_vendas 
+ON vendas 
+FOR INSERT
+AS
+BEGIN
+	DECLARE
+    @data  DATETIME,
+	-- Criação da tabela temporária 'inserted' que recebe os dados temporariamente da tabela 'vendas' em uma linha
+	@valor DECIMAL(10, 2) 
+    
+	
+	-- As variáveis recebem aqueles valores durante a seleção*/
+	SELECT @data = data, @valor = valor FROM inserted  
+
+	-- Em seguida os valores das variávies são utilizados para fazer o update*/
+	UPDATE caixa SET saldo_final = saldo_final + @valor 
+    WHERE data = @data
+END
+GO
+  
+INSERT INTO vendas(data, cod_venda, valor) VALUES (CONVERT(VARCHAR, CONVERT(DATETIME,GETDATE()), 120)   , 1, 20) 
+GO
+
+SELECT * FROM vendas;
+SELECT * FROM caixa;
+
