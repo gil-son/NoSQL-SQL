3ab3c04e94d47283ed4f652d42825aa6798dd1d7
gil-son <gilszon@gmail.com>
2021-01-02 08:35:36 -0300

    Hello to user(s)

---
+++ SQL/MySQL/Functions/function_hello.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE Company;
+USE Company;
+
+CREATE TABLE users
+(
+	id_user INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_name VARCHAR(50)
+);
+
+INSERT INTO users(user_name) VALUES ('AAA'), ('BBB'), ('CCC'), ('DDD'), ('EEE');
+
+# https://stackoverflow.com/questions/26015160/deterministic-no-sql-or-reads-sql-data-in-its-declaration-and-binary-logging-i
+# SET GLOBAL log_bin_trust_function_creators = 1;
+
+CREATE FUNCTION hello(a VARCHAR(50)) RETURNS VARCHAR(50) RETURN CONCAT('Hello, ', a, '!');
+
+SELECT hello(user_name) as `Welcome` FROM users;
+
+
+
+
