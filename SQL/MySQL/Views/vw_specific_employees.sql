b02ba896fa7af07c681c41b259da931d143f985c
gil-son <gilszon@gmail.com>
2021-01-03 06:58:57 -0300

    Count specific employees

---
+++ SQL/MySQL/Views/vw_specific_employees.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE Company;
+USE Company;
+
+CREATE TABLE employee
+(
+	id_employee INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    employee_name VARCHAR(50)
+);
+
+INSERT INTO employee(employee_name) VALUES ('AAA'), ('BBB'), ('CCC'), ('DDD'), ('EEE');
+
+CREATE VIEW vw_specific_employees AS 
+SELECT count(id_employee) AS `Total A + B` FROM employee WHERE employee_name LIKE '%A%' OR employee_name LIKE '%B%'; 
+			  
+SELECT * FROM vw_specific_employees;
+
